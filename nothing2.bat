607661af7927532cf4c3cd4738cd5a01826e306f
FearOffTheFart <62621595+FearOffTheFart@users.noreply.github.com>
2020-03-25 16:00:57 +0100

    Update nothing2.bat

--- nothing2.bat
+++ nothing2.bat
@@ -6,7 +6,7 @@ powershell -Command "Invoke-WebRequest http://art-exlibris.net/images/large/exli
 timeout 1
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /f /d %temp%\hatter.jpg
-reg add "HKEY_CURRENT_USER\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+::reg add "HKEY_CURRENT_USER\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
 rundll32.exe user32.dll, UpdatePerUserSystemParameters
 timeout 3
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/FearOffTheFart/Bash/master/clownlaugh.mp3 -OutFile %temp%\asd.mp3"
